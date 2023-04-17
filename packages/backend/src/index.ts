a2b13ad29d62d5e1bb34f1baeeb41a2779d5a1d2
Gustavo Barska <gbarska@ibiunainvest.com.br>
2023-04-17 11:24:40 -0300

    Adjusting backend package.json type

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -3,3 +3,9 @@ import { createUser, showUser, User } from '@monorepo/shared';
 const user: User = createUser('gbarska', 18);
 
 showUser(user);
+
+function test(){
+    console.log('test');
+}
+
+test();
